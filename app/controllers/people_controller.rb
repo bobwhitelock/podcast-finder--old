0a850ddef96a4c625b2649b6afa3c47f0c9fff8a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-09-30 02:58:41 +0100

    Handle extracting API response parts in controller
    Rather than doing this in view.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,7 +8,25 @@ class PeopleController < ApplicationController
 
   def search_people(name)
     result = audiosearch_client.search({ q: name }, 'people')
-    @people = result.results
+    @people = result.results.map { |p| parse_person_result(p) }
+  end
+
+  def parse_person_result(person_result)
+    OpenStruct.new({
+      name: person_result.name,
+      episodes: person_result.episodes.map { |e| parse_episode_result(e.episode) }
+    })
+  end
+
+  def parse_episode_result(episode_result)
+    OpenStruct.new({
+      player_url: episode_result.urls.ui,
+      # `gsub` to get the full image URL instead of low resolution thumbnail.
+      image_url: episode_result.thumb_image.gsub('thumb_', ''),
+      show_title: episode_result.show_title,
+      date: episode_result.date_created,
+      title: episode_result.title,
+    })
   end
 
   def audiosearch_client
