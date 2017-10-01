31a0c8391bf34b6baaff8371999054fbee83809a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-01 02:01:52 +0100

    Remove unnecessary searching by people
    Doesn't appear to give better results than just searching by episode,
    and doesn't know about many people or episodes, so just searching by
    episodes seems best for now.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,6 @@ class PeopleController < ApplicationController
   def search
     @name = params[:name]
     search_episodes(@name)
-    search_people(@name)
   end
 
   private
@@ -12,23 +11,10 @@ class PeopleController < ApplicationController
     @episodes = result.results.map { |e| parse_episode_result(e) }
   end
 
-  def search_people(name)
-    result = audiosearch_client.search({ q: name }, 'people')
-    @people = result.results.map { |p| parse_person_result(p) }
-  end
-
-  def parse_person_result(person_result)
-    OpenStruct.new({
-      name: person_result.name,
-      episodes: person_result.episodes.map { |e| parse_episode_result(e.episode) }
-    })
-  end
-
   def parse_episode_result(episode_result)
     OpenStruct.new({
       player_url: episode_result.urls.ui,
-      # `gsub` to get the full image URL instead of low resolution thumbnail.
-      image_url: episode_result.thumb_image&.gsub('thumb_', '') || episode_result.image_urls.full,
+      image_url: episode_result.image_urls.full,
       show_title: episode_result.show_title,
       date: episode_result.date_created,
       title: episode_result.title,
